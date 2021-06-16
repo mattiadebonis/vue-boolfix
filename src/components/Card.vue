e5f6cf5edb67845466e769f0b1a44cb96bcd5b65
mattiadebonis <mattia.debonis@gmail.com>
2021-06-16 17:07:00 +0200

    Visualizzazione lingua film

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,6 +2,20 @@
     <div>
         <h3>{{item.title}}</h3>
         <h4>{{item.original_title}}</h4>
+        <img 
+            src="../assets/images/it.png" 
+            alt=""
+            v-if="item.original_language == 'it'"
+        >
+        <img 
+            src="../assets/images/eng.png" 
+            alt=""
+            v-else-if="item.original_language == 'eng'"
+        >
+        <p v-else>{{item.original_language}}</p>
+        <img src="" alt="">
+        <p></p>
+
         <p>{{item.original_language}}</p>
         <p>{{item.vote_average}}</p>
     </div>
